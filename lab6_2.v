e8e04f0e221ceda360ed4cd52fa3a4bc716315db
Jacob Lin <lovetaeyeon507@gmail.com>
2021-04-30 02:01:38 +0800

    first commit

---
+++ lab6_2.v
@@ -0,0 +1,8 @@
+module FPGA_practice2(led0, led2, in0, in1, in2);
+
+    input in0, in1, in2;
+    output led0, led1;
+
+    assign led0 = ~in2 | (in2&in1& ~in0);
+    assign led1 = ~in2 | (in2 & ~in1);
+endmodule
